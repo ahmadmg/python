f348134885e526e58f9f0f8161bfce905a22fdaf
ahmadmg <ahmadmg@gmail.com>
2025-09-01 12:23:09 +0200

    l

--- PythonSeleniumProject1/LearningSelenium/FirstAutomationTest.py
+++ PythonSeleniumProject1/LearningSelenium/FirstAutomationTest.py
@@ -1,13 +1,8 @@
-import time
-
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-chrome_driver_path = "C:\\browserdriver\\chrome-win64\\chrome.exe"
-service_object = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service_object)
-driver.get("https://www.python.org")
-print(driver.title)
-time.sleep(10)
 
-driver.close()
+# Selenium will automatically handle downloading the correct driver
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
