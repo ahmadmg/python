05b0419e5879c5f7924e39415e903ee71f43784c
ahmadmg <ahmadmg@gmail.com>
2025-09-12 12:42:30 +0200

    Escape Sequences Characters

--- HELLOWORLD/variables.py
+++ HELLOWORLD/variables.py
@@ -33,8 +33,50 @@ print(my_name)
 # Reserved Words
 help("keywords")
 
-a, b, c = 1, 2, 'Hello world'
+a, b, c = 1, 2, 'Next'
 
 print(a)
 print(b)
 print(c)
+
+# ----------------------------
+# Escape Sequences Characters
+# \b => Back Space
+# \newline => Escape New Line + \
+# \\ => Escape Back Slash
+# \' => Escape Single Quotes
+# \" => Escape Double Quotes
+# \n => Line Feed
+# \r => Carriage Return
+# \t => Horizontal Tab
+# \xhh => Character Hex Value
+# ----------------------------
+
+# Back Space
+print("Hello\bWorld")  # Will Remove o
+
+# Escape New Line + Back Slash
+print("Hello \
+I Love \
+Python")
+
+# Escape Back Slash
+print("I Love Back Slash \\")
+
+# Escape Single Quote
+print('I Love Single Quote \'Test\' ')
+
+# Escape Double Quotes
+print("I Love Double Quotes \"Test\" ")
+
+# Line Feed
+print("Hello World\nSecond Line")
+
+# Carriage Return
+print("123456\rAbcde")
+
+# Horizontal Tab
+print("Hello\tPython")
+
+# Character Hex Value
+print("\x4F\x73")
