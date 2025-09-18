2b1fa16534112e4566c6ba375ec9f5274a7d319e
ahmadmg <ahmadmg@gmail.com>
2025-09-18 14:34:20 +0200

    .

--- HELLOWORLD/stringsMethods.py
+++ HELLOWORLD/stringsMethods.py
@@ -177,3 +177,18 @@ u = "AaaaaBbbbbb"
 z = "AaaaaBbbbbb111"
 print(u.isalnum())
 print(z.isalnum())
+
+# replace(Old Value, New Value, Count)
+
+a = "Hello One Two Three One One"
+print(a.replace("One", "1"))
+print(a.replace("One", "1", 1))
+print(a.replace("One", "1", 2))
+
+# join(Iterable)
+
+myList = ["Osama", "Mohamed", "Elsayed"]
+print("-".join(myList))
+print(" ".join(myList))
+print(", ".join(myList))
+print(type(", ".join(myList)))
